1a0beadfe994ca526587a421a522f9cb134fcc73
HaroldGaspar <gaspar.hrl@gmail.com>
2021-10-08 02:24:43 -0500

    rm policy

--- api/product/config/policies/is-user.js
+++
@@ -1,9 +0,0 @@
-module.exports = async (ctx, next) => {
-  if (ctx.state.user?.role.name === "Administrator") {
-    // Go to next policy or will reach the controller's action.
-    return await next();
-  }
-  console.log("role: ", ctx.state.user | "ctx.state.user");
-  //   ctx.unauthorized(`You're not allowed to perform this action!`);
-  return await next();
-};
