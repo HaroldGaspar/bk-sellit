7acee1214fe7403c9a96b5f99787dc28c154d33b
HaroldGaspar <gaspar.hrl@gmail.com>
2021-10-27 19:21:23 -0500

    8000

--- config/server.js
+++ config/server.js
@@ -1,6 +1,6 @@
 module.exports = ({ env }) => ({
   host: env("HOST", "0.0.0.0"),
-  port: env.int("PORT", 80),
+  port: env.int("PORT", 8000),
   admin: {
     auth: {
       secret: env("ADMIN_JWT_SECRET", "2e9baec0d8ca88e73aeb2a29e11ba544"),
